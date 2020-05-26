790c18a697646daea87962365e97a819586a5288
yanghd <yanghd@glodon.com>
2020-05-26 10:46:58 +0800

    xfire base

---
+++ josbParent/base-service/src/main/java/com/practice/service/dao/TeacherDao.java
@@ -0,0 +1,71 @@
+package com.practice.service.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.practice.service.config.DatabaseConnectionService;
+import com.practice.service.domain.Teacher;
+
+public class TeacherDao {
+
+	private static DatabaseConnectionService connectionService = new DatabaseConnectionService();
+
+	public void save(Teacher techer) {
+		String sql = "INSERT INTO `teacher` (`wNo`, `name`, `major`,`hiredate`)VALUES(?,?,?,?)";
+		sql.replaceFirst("\\?", techer.getwNo());
+		sql.replaceFirst("\\?", techer.getName());
+		sql.replaceFirst("\\?", techer.getMajor());
+		sql.replaceFirst("\\?", techer.getHiredate());
+
+		Connection conn = connectionService.getConnection();
+		try {
+			PreparedStatement pst = conn.prepareStatement(sql);
+			pst.execute();
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				conn.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+
+	public List<Teacher> getTeachers() {
+		// TODO Auto-generated method stub
+		String sql = "select * from teacher";
+		Connection conn = connectionService.getConnection();
+		List<Teacher> teachers = new ArrayList<Teacher>();
+		try {
+			PreparedStatement pst = conn.prepareStatement(sql);
+			ResultSet rs = pst.executeQuery();
+
+			while (rs.next()) {
+				Teacher teacher = new Teacher();
+				teacher.setMajor(rs.getString("major"));
+				teacher.setHiredate(rs.getString("hiredate"));
+				teacher.setName(rs.getString("name"));
+				teacher.setwNo(rs.getString("wNo"));
+				teachers.add(teacher);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				conn.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+
+		return teachers;
+	}
+
+}
--- josbParent/base-service/src/main/java/com/practice/service/impl/TeacherServiceImpl.java
+++ josbParent/base-service/src/main/java/com/practice/service/impl/TeacherServiceImpl.java
@@ -1,74 +1,25 @@
 package com.practice.service.impl;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-
-
-
-
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.List;
-
-import org.springframework.stereotype.Service;
 
 import com.practice.service.TeacherService;
 import com.practice.service.config.DatabaseConnectionService;
+import com.practice.service.dao.TeacherDao;
 import com.practice.service.domain.Teacher;
 
-@Service
 public class TeacherServiceImpl implements TeacherService {
-	private static DatabaseConnectionService connectionService = new DatabaseConnectionService(); 
+	private static DatabaseConnectionService connectionService = new DatabaseConnectionService();
 
-	  @Override
-	  public void save(Teacher techer) {
-	    String sql = "INSERT INTO `teacher` (`wNo`, `name`, `major`,`hiredate`)VALUES(?,?,?,?)";
-	    sql.replaceFirst("\\?", techer.getwNo());
-	    sql.replaceFirst("\\?", techer.getName());
-	    sql.replaceFirst("\\?", techer.getMajor());
-	    sql.replaceFirst("\\?", techer.getHiredate());
-	    
-	    Connection conn = connectionService.getConnection();
-	    try{
-		    PreparedStatement pst = conn.prepareStatement(sql);
-		    pst.execute();
-	    }catch(Exception e){
-	    	e.printStackTrace();
-	    }
-	  }
-	  
-	  @Override
-		public List<Teacher> getTeachers() {
-			// TODO Auto-generated method stub
-			String sql = "select * from teacher";
-			Connection conn = connectionService.getConnection();
-			List<Teacher> teachers = new ArrayList<Teacher>();
-			try{
-				PreparedStatement pst = conn.prepareStatement(sql);
-				ResultSet rs = pst.executeQuery();
-				
-		        while (rs.next()) {
-		        	Teacher teacher = new Teacher();
-					teacher.setMajor(rs.getString("major"));
-					teacher.setHiredate(rs.getString("hiredate"));
-					teacher.setName(rs.getString("name"));
-					teacher.setwNo(rs.getString("wNo"));
-					teachers.add(teacher);
-		        }
-			}catch(Exception e) {
-				e.printStackTrace();
-			}finally{
-				try {
-					conn.close();
-				} catch (SQLException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-			}
-			
-			return teachers;
-		}
+	private TeacherDao techerDao = new TeacherDao();
 
+	@Override
+	public void save(Teacher techer) {
+		techerDao.save(techer);
+	}
 
+	@Override
+	public List<Teacher> getTeachers() {
+		// TODO Auto-generated method stub
+		return techerDao.getTeachers();
+	}
 }
